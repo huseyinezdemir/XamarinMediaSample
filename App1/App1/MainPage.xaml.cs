fc023ba0535e01ab0741a317abf78299895ec7a7
Hüseyin Özdemir <hozdemir@nuevoyazilim.com>
2018-03-09 23:31:22 +0300

    initialize

---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var result = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions
+                {
+                    SaveToAlbum = true,
+                    PhotoSize = PhotoSize.Small
+                });
+                string pat = result.Path;
+            });
+
+
+        }
+    }
+}
